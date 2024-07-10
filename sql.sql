d0121fb01983b1a888f0424bdedf2b8d7dd3c584
yogisonawane47 <sonawaneyogi12@gmail.com>
2024-07-10 06:26:04 -0700

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,110 @@
+show databases;
+desc empn;
+desc stud_details;
+alter table stud_details add courseid int,add course_name varchar(20);
+desc stud_details;
+alter table stud_details rename to stud;
+desc stud;
+alter table stud modify column address varchar(100) after studroll;
+desc stud;
+
+alter table stud add primary key (studroll);
+
+show tables;
+desc stud;
+
+alter table stud drop primary key;
+desc stud;
+
+alter table stud add primary key (studroll);
+desc stud;
+
+create table course(cid int,cname varchar(20),studroll int);
+
+alter table course add constraint stud_fix foreign key(studroll)
+references stud(studroll);
+
+desc course;
+
+
+create database companyys;
+
+use companyys;
+
+create table product_details(prod_id int primary key,prod_name 
+varchar(20),prod_cat varchar(20),prod_price decimal(10,2));
+
+desc product_details;
+
+insert into product_details values(1,"mobile","electronics",20000);
+
+insert into product_details values(2,"sofaset","furniture",25000);
+
+insert into product_details values(3,"refrigerator","electronics",30000);
+
+insert into product_details values(4,"cooker","kitchen applinces",2000);
+
+insert into product_details values(5,"bat","sports acupment",2000);
+
+insert into product_details values(6,"chair","furniture",3000);
+
+insert into product_details values(7,"sofaset","furniture",25000);
+
+select*from product_details;
+
+select prod_name,prod_price from product_details;
+
+select 4+4;
+
+select 4+5 as result;
+
+select distinct prod_name from product_details;
+
+select * from product_details where prod_cat="electronics";
+
+select * from product_details where prod_price<10000;
+
+select * from product_details where prod_price>10000;
+
+select distinct prod_name,prod_name,prod_cat,prod_price
+ from product_details where prod_price<10000;
+
+select * from product_details where prod_cat="electronics" and prod_price>25000;
+
+select 125-120 as "result";
+
+select 2*4 as "result";
+
+select *  from product_details;
+
+select prod_price-1000 as "discount" from product_details;
+
+select prod_price-1000 as "discount" from product_details where prod_name="mobile";
+
+select * from product_details;
+
+select * from product_details where prod_cat="electronics" or prod_price>20000;
+
+select * from product_details where  prod_price between 1000 and 3000;
+
+select * from product_details where  prod_price not between 1000 and 3000;
+
+select * from product_details where  prod_name in ('mobile','sofaset');
+
+select * from product_details where  prod_name not in ('mobile','sofaset');
+
+select * from product_details where prod_cat like 'e%';
+
+select * from product_details where prod_name like 'mobil_';
+
+select * from product_details order by prod_price;
+
+select * from product_details order by prod_name;
+
+select * from product_details limit 5;
+
+select * from product_details limit 4;
+
+select * from product_details limit 2,5;  do ke age ke pach recoard
+
+
